d37c31ecb1f5613ea1b93319f3c0f58584baf206
Piotr Wasiak <chooosen@gmail.com>
2019-07-17 00:06:22 +0200

    Switch to unit test

--- prodenum/prodenum.rb
+++ prodenum/prodenum.rb
@@ -1,72 +1,74 @@
-def primitives(n)
-  temp_table = Array.new(n + 1, true)
-  (2..n).to_a.each do |number|
-    unprimitive_number = number + number
-    while unprimitive_number <= n
-      temp_table[unprimitive_number] = false
-      unprimitive_number += number
+# module Solution
+  def primitives(n)
+    temp_table = Array.new(n + 1, true)
+    (2..n).to_a.each do |number|
+      unprimitive_number = number + number
+      while unprimitive_number <= n
+        temp_table[unprimitive_number] = false
+        unprimitive_number += number
+      end
+    end
+    (2..temp_table.size).to_a.select { |i| temp_table[i] }
+  end
+
+  # -------------
+  # enum() is not worth to generate with 1 and not primitives numbers if we wanna product them
+  # (which means multiply)
+  # So we need to store just primitives elements, which in sum gives us `n`.
+  # Lower combinations of sum elements will be also included in list of `n`
+  # e.g 2*3 + 1 belongs to prod(enum(`6`))
+  # We can use lower `n` results to generate new one (but I won't explain how to do it :)
+  # I optimized generation part to get things on time - but I know it can be done better
+  # (I need to use uniq for some cases)
+
+  def prodenum(n)
+    $primitives ||= primitives(90)
+    $new_number_products_cache ||= Array.new(4, [])
+
+    result = (4..n).to_a.flat_map { |i| primitives_combinations(i) }
+    (result | [1].concat($primitives.select { |pr| pr <= n })).sort
+  end
+
+  # split into 2 sum elemenets and
+  # combine them with previous calculated sum primitives or just with primitive
+  def primitives_combinations(n)
+    $new_number_products_cache[n] ||= begin
+      (2..n / 2).flat_map do |z|
+        lower_part = wrap_array_for_primitives_or_new_number_product(z)
+        higher_part = wrap_array_for_primitives_or_new_number_product(n - z)
+        higher_part.product(lower_part).map { |combination| combination.reduce(&:*) }
+      end.uniq
     end
   end
-  (2..temp_table.size).to_a.select { |i| temp_table[i] }
-end
 
-# -------------
-# enum() is not worth to generate with 1 and not primitives numbers if we wanna product them
-# (which means multiply)
-# So we need to store just primitives elements, which in sum gives us `n`.
-# Lower combinations of sum elements will be also included in list of `n`
-# e.g 2*3 + 1 belongs to prod(enum(`6`))
-# We can use lower `n` results to generate new one (but I won't explain how to do it :)
-# I optimized generation part to get things on time - but I know it can be done better
-# (I need to use uniq for some cases)
-
-def prodenum(n)
-  $primitives ||= primitives(90)
-  $new_number_products_cache ||= Array.new(4, [])
-
-  result = (4..n).to_a.flat_map { |i| primitives_combinations(i) }
-  (result | [1].concat($primitives.select { |pr| pr <= n })).sort
-end
-
-# split into 2 sum elemenets and
-# combine them with previous calculated sum primitives or just with primitive
-def primitives_combinations(n)
-  $new_number_products_cache[n] ||= begin
-    (2..n / 2).flat_map do |z|
-      lower_part = wrap_array_for_primitives_or_new_number_product(z)
-      higher_part = wrap_array_for_primitives_or_new_number_product(n - z)
-      higher_part.product(lower_part).map { |combination| combination.reduce(&:*) }
-    end.uniq
+  def wrap_array_for_primitives_or_new_number_product(number)
+    primitive?(number) ? [number] : primitives_combinations(number)
   end
-end
 
-def wrap_array_for_primitives_or_new_number_product(number)
-  primitive?(number) ? [number] : primitives_combinations(number)
-end
-
-def primitive?(number)
-  $primitives.include? number
-end
-
-# ----------------- output 3rd step
-
-def part(n)
-  array = prodenum(n)
-  avg = array.sum / array.size.to_f
-  range = array.last - array.first
-  median = median(array)
-  "Range: #{range} Average: #{format_float(avg)} Median: #{format_float(median)}"
-end
-
-def median(array)
-  half_size = array.size / 2
-  if array.size.even?
-    (array[half_size - 1] + array[half_size]) / 2.0
-  else
-    array[half_size]
+  def primitive?(number)
+    $primitives.include? number
   end
-end
 
-def format_float(value)
-  format('%.2f', value).to_s
-end
+  # ----------------- output 3rd step
+
+  def part(n)
+    array = prodenum(n)
+    avg = array.sum / array.size.to_f
+    range = array.last - array.first
+    median = median(array)
+    "Range: #{range} Average: #{format_float(avg)} Median: #{format_float(median)}"
+  end
+
+  def median(array)
+    half_size = array.size / 2
+    if array.size.even?
+      (array[half_size - 1] + array[half_size]) / 2.0
+    else
+      array[half_size]
+    end
+  end
+
+  def format_float(value)
+    format('%.2f', value).to_s
+  end
+# end
--- test.rb
+++ test.rb
@@ -1,63 +1,59 @@
 require "test/unit"
-
+require 'active_support'
 # ============================ Test suite to test locally codewars
 # To run type in shell i.e. `ruby prodenum/spec.rb`
 # In that file require_relative ../test.rb and your solution + paste specs
-class Test < Test::Unit::TestCase
-  GREEN_COLOR = "\e[0;32m".freeze
-  RED_COLOR = "\e[0;31m".freeze
-  FINISH_COLOR = "\e[0m".freeze
+module Codewars
+  class Test < Test::Unit::TestCase
+    GREEN_COLOR = "\e[0;32m".freeze
+    RED_COLOR = "\e[0;31m".freeze
+    FINISH_COLOR = "\e[0m".freeze
 
-  def self.diff(first, second)
-    first_s = first.split(' ')
-    second_s = second.split(' ')
-    common_part = second_s & first_s
-    first_s.map do |word|
-      color = common_part.include?(word) ? GREEN_COLOR : RED_COLOR
-      [color, word, FINISH_COLOR].join
-    end.join(' ')
-  end
+    alias :assert_equals :assert_equal
 
-  def self.assert_equals(test, expected)
-    if test == expected
-      puts "#{GREEN_COLOR}  #{expected} #{FINISH_COLOR}"
-    else
-      puts "#{RED_COLOR}  Expected was: #{expected}",
-           "  Resultat was: #{FINISH_COLOR}#{diff(test, expected)}",
-           caller(0, 2)
+    def self.diff(first, second)
+      first_s = first.split(' ')
+      second_s = second.split(' ')
+      common_part = second_s & first_s
+      first_s.map do |word|
+        color = common_part.include?(word) ? GREEN_COLOR : RED_COLOR
+        [color, word, FINISH_COLOR].join
+      end.join(' ')
     end
-  end
 
-  def self.describe(description, &block)
-    Benchmark.bm do |benchmark|
-      @@benchmark = benchmark
+    def self.describe(description, &block)
       @@description = description
       puts 'Testing ' + description
 
       block.call
+
+      @@description = ''
     end
-  end
 
-  def self.benchmark
-    @@benchmark
-  end
+    def self.description
+      @@description
+    end
 
-  def self.description
-    @@description
-  end
+    def self.assert_equals(test, expected, description = nil)
+      name = Random.urandom(10).chars.map { |c| (c.ord % 25 + 97).chr }.join
+      test_description = [self.description, self.it_description, description].compact.join(': ')
+      define_method('test_' + name) { assert_equals(test, expected, test_description) }
+    rescue => e
+      puts "#{RED_COLOR} Error occured: #{e} #{FINISH_COLOR}", e.backtrace[0..1]
+    end
 
-  def self.it(description, &block)
-    @@it_description = description
-    benchmark.report(' ' + it_description + "\n") do
+    def self.it(description, &block)
+      @@it_description = description
       begin
         block.call
       rescue => e
         puts "#{RED_COLOR} Error occured: #{e} #{FINISH_COLOR}", e.backtrace[0..1]
       end
+      @@it_description = ''
     end
-  end
 
-  def self.it_description
-    @@it_description
+    def self.it_description
+      @@it_description
+    end
   end
 end
